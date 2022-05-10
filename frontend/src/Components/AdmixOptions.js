79b3966f63001479867ed90881023dd84278aded
Keivin98 <kevinisufaj@gmail.com>
2022-05-10 13:54:48 +0300

    added certainty to admixture option

--- frontend/src/Components/AdmixOptions.js
+++ frontend/src/Components/AdmixOptions.js
@@ -1,66 +1,67 @@
-import React, { Component } from 'react';
-import Typography from '@material-ui/core/Typography';
-import Slider from '@material-ui/core/Slider';
-import { Button } from '@material-ui/core';
-import PropTypes from 'prop-types';
+import React, { Component } from "react";
+import Typography from "@material-ui/core/Typography";
+import Slider from "@material-ui/core/Slider";
+import { Button } from "@material-ui/core";
+import PropTypes from "prop-types";
 
 class AdmixOptions extends Component {
-	state = {
-		columnRange: this.props.columnRange,
-	};
+  state = {
+    initialVal: this.props.initialVal,
+  };
 
-	rangeSelector = (event, newValue) => {
-		this.setState({ columnRange: newValue });
-	};
+  rangeSelector = (event, newValue) => {
+    this.setState({ initialVal: newValue });
+  };
 
-	render() {
-		return (
-			<div
-				style={{
-					display: 'flex',
-					flexDirection: 'column',
-					width: '100%',
-					justifyContent: 'space-between',
-					marginTop: '10%',
-					marginBottom: '5%',
-					height: '50px',
-					paddingTop: '10px',
-				}}
-			>
-				<Typography id="range-slider" gutterBottom>
-					<h6>Alpha: </h6>
-				</Typography>
-				<div
-					style={{
-						display: 'flex',
-						flexDirection: 'row',
-						justifyContent: 'space-around',
-					}}
-				>
-					<Slider
-						value={this.state.columnRange}
-						onChange={this.rangeSelector}
-						valueLabelDisplay="auto"
-						min={0}
-						max={100}
-					/>
-					<Button
-						onClick={(event) => {
-							this.props.parentCallback(this.state.columnRange);
-							event.preventDefault();
-						}}
-						variant="outlined"
-						style={{ marginLeft: '10px' }}
-					>
-						Apply
-					</Button>
-				</div>
-				Chosen alpha: {this.state.columnRange} %
-			</div>
-		);
-	}
+  render() {
+    return (
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          width: "100%",
+          justifyContent: "space-between",
+          marginTop: "10%",
+          marginBottom: "5%",
+          height: "50px",
+          paddingTop: "10px",
+        }}
+      >
+        <Typography id="range-slider" gutterBottom>
+          <h6>{this.props.name}: </h6>
+        </Typography>
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "row",
+            justifyContent: "space-around",
+          }}
+        >
+          <Slider
+            value={this.state.initialVal}
+            onChange={this.rangeSelector}
+            valueLabelDisplay="auto"
+            min={0}
+            max={100}
+          />
+          <Button
+            onClick={(event) => {
+              this.props.parentCallback(this.state.initialVal);
+              event.preventDefault();
+            }}
+            variant="outlined"
+            style={{ marginLeft: "10px" }}
+          >
+            Apply
+          </Button>
+        </div>
+        Chosen {this.props.name}: {this.state.initialVal} %
+      </div>
+    );
+  }
 }
 AdmixOptions.propTypes = {
-	columnRange: PropTypes.number,
+  initialVal: PropTypes.number,
+  name: PropTypes.string,
 };
 export default AdmixOptions;
