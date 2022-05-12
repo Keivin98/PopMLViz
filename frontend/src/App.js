cb3c3d708f8b8f1245ec8db6cce8ac4c41925c25
Keivin98 <kevinisufaj@gmail.com>
2022-05-12 12:56:53 +0300

    changed name of main file

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,31 +1,31 @@
-import './App.css';
-import UploadFile from './Components/UploadFile';
-import Navbar from 'react-bootstrap/Navbar';
+import "./App.css";
+import Dashboard from "./Components/Dashboard";
+import Navbar from "react-bootstrap/Navbar";
 
 function App() {
-	return (
-		<div>
-			<Navbar
-				style={{
-					position: 'fixed',
-					height: '6%',
-					width: '100%',
-					paddingLeft: '45%',
-					backgroundColor: '#3b3f4e',
-				}}
-			>
-				<Navbar.Brand style={{ color: 'white' }}>
-					PopMLViz
-					<img
-						src="./logo.jpeg"
-						style={{ width: '6%', position: 'fixed', left: '7%', top: '1%' }}
-					/>
-				</Navbar.Brand>
-			</Navbar>
+  return (
+    <div>
+      <Navbar
+        style={{
+          position: "fixed",
+          height: "6%",
+          width: "100%",
+          paddingLeft: "45%",
+          backgroundColor: "#3b3f4e",
+        }}
+      >
+        <Navbar.Brand style={{ color: "white" }}>
+          PopMLViz
+          <img
+            src="./logo.jpeg"
+            style={{ width: "6%", position: "fixed", left: "7%", top: "1%" }}
+          />
+        </Navbar.Brand>
+      </Navbar>
 
-			<UploadFile />
-		</div>
-	);
+      <Dashboard />
+    </div>
+  );
 }
 
 export default App;
