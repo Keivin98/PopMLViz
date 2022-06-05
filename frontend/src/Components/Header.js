1a66d4c380452e0f174a3acb5565717f3cb2d163
Keivin98 <kevinisufaj@gmail.com>
2022-06-05 13:05:12 +0300

    changed the style of the landing page

--- frontend/src/Components/Header.js
+++ frontend/src/Components/Header.js
@@ -5,9 +5,10 @@ import useAnalyticsEventTracker from "./useAnalyticsEventTracker";
 import { FaBook, FaGithub, FaPlayCircle } from "react-icons/fa";
 import "./headerFP.css";
 import PopMLvis from "../assets/PopMLvis.pdf";
+
 function Header(props) {
   const gaEventTracker = useAnalyticsEventTracker("social");
-  const name = "PopMLViz";
+  const name = "PopMLVis";
   const bio =
     "PopMLvis is a population genetic analysis application. We provide two versions of PopMLvis; a web-based (online) and computer-based (offline) application. It provides a comprehensive interactive environment for scientists, bioinformaticians, and researchers to dig deeper in analyzing population genetic datasets. In order to understand the gene structure, our platform analysis includes dimensionality reduction algorithms, machine learning models, statistical measurements.";
   return (
@@ -37,8 +38,25 @@ function Header(props) {
 
       <div className="row banner">
         <div className="banner-text">
-          <h1 className="responsive-headline">{name}</h1>
-          <h2 style={{ fontWeight: "200", fontSize: 24 }}>{bio}.</h2>
+          <h1
+            className="responsive-headline"
+            style={{
+              fontFamily: "Arial Black",
+              color: "#071b52",
+            }}
+          >
+            {name}
+          </h1>
+          <mark
+            style={{
+              fontWeight: "200",
+              fontSize: 24,
+              backgroundColor: "white",
+              color: "black",
+            }}
+          >
+            {bio}.
+          </mark>
           <hr />
 
           <ul className="social">
@@ -52,6 +70,7 @@ function Header(props) {
             <a
               href={"https://github.com/qcri/QCAI-PopMLVis"}
               className="button btn github-btn"
+              style={{ color: "white" }}
               onClick={() => gaEventTracker("Github")}
             >
               <FaGithub />
@@ -61,7 +80,7 @@ function Header(props) {
               href={PopMLvis}
               className="button btn project-btn"
               style={{ marginLeft: 50 }}
-              onClick={() => gaEventTracker("Github")}
+              onClick={() => gaEventTracker("Docs")}
             >
               <FaBook />
               Docs
