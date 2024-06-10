dbed29775816fe3b2732765735e2aa4afa675f20
ibrahim-Alasalimy <111947333+ibrahim-Alasalimy@users.noreply.github.com>
2024-06-10 14:23:58 +0300

    media queries for mobile view in homepage

--- frontend/src/Components/landingPage/Components/Header.js
+++ frontend/src/Components/landingPage/Components/Header.js
@@ -1,7 +1,8 @@
 import React, { useState } from "react";
 import ParticlesBg from "particles-bg";
 import useAnalyticsEventTracker from "../../useAnalyticsEventTracker";
-import { FaPlayCircle } from "react-icons/fa";
+import { FaPlayCircle, FaBook, FaGithub } from "react-icons/fa";
+import { LiaQuestionSolid } from "react-icons/lia";
 import "./headerFP.css";
 import logo from "../../../assets/logo.jpeg";
 import PopMLvis from "../../../assets/PopMLvis.pdf";
@@ -86,8 +87,40 @@ function Header(props) {
 
           <ul className="social">
             <a href={"/Dashboard"} style={{}} className="button btn project-btn" onClick={() => gaEventTracker("Live")}>
-              <FaPlayCircle /> Start Visualizing
+              <FaPlayCircle style={{ marginRight: 10 }} /> Start Visualizing
             </a>
+            <div className="spcial-btn-container">
+              <a
+                href={PopMLvis}
+                className="button btn FAQ-btn special-btn"
+                style={{ color: "white" }}
+                onClick={() => gaEventTracker("Docs")}
+              >
+                <img style={{width:"60px", height:"60px"}} src="/faq.png"></img>
+                <div className="button-txt">FAQ</div>
+              </a>
+              <a
+                href={PopMLvis}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="button btn docks-btn special-btn"
+                style={{ color: "white" }}
+                onClick={() => gaEventTracker("Docs")}
+              >
+                <FaBook size={35} />
+                <div className="button-txt"> Docs</div>
+              </a>
+              <a
+                className="button btn github-btn special-btn"
+                href="https://github.com/qcri/QCAI-PopMLVis"
+                target="_blank"
+                rel="noopener noreferrer"
+                onClick={() => gaEventTracker("Github")}
+              >
+                <FaGithub size={35}/>
+                <div className="button-txt">Github</div>
+              </a>
+            </div>
           </ul>
         </div>
       </div>
